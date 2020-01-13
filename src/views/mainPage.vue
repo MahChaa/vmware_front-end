5b23719b418d72af827992964885fa24c1ff514a
Mahdi Chaari <mahdi-chaari@hotmail.com>
2020-01-13 00:28:40 -0500

    Created basic tab layout for the 2 pages required.

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -1,12 +1,23 @@
 <template>
-    <v-container style="background-color: blue; font-size: 25px;">
-        Hello World
-    </v-container>
+    <div>
+<!--        TODO:This works, but think about making it more modular by using routing capabilities-->
+        <v-tabs v-model="tabs" dark slider-color="blue darken-1">
+            <v-tab>Page 1</v-tab>
+            <v-tab>Page 2 & 3</v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tabs">
+            <v-tab-item>YOYOYO</v-tab-item>
+            <v-tab-item>YEYEYE</v-tab-item>
+        </v-tabs-items>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "mainPage"
+        name: "mainPage",
+        data: () => ({
+            tabs: null
+        })
     }
 </script>
 
